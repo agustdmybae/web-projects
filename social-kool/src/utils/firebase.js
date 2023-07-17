4c160f65b87cbe67bfb876a9d8d9a49e6d2565e7
Chi Chen <stephchen55@gmail.com>
2023-07-17 20:12:53 +0800

    firebase 初始化

---
+++ social-kool/src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqobTACLdjH33R76Esy1k0869PE2tGtG4",
+  authDomain: "social-kool.firebaseapp.com",
+  projectId: "social-kool",
+  storageBucket: "social-kool.appspot.com",
+  messagingSenderId: "302910590594",
+  appId: "1:302910590594:web:93dd24678ffe5dc747ee9b"
+};
+
+firebase.initializeApp(firebaseConfig);
