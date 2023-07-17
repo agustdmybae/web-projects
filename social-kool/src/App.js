e97708ea9a18168f1f91ce6a625d4ae93bea1da5
Chi Chen <stephchen55@gmail.com>
2023-07-18 01:31:44 +0800

    add Home.js

--- social-kool/src/App.js
+++ social-kool/src/App.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import React from 'react';
 import Signin from './pages/Signin'
+import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -8,7 +9,7 @@ export default function App() {
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path="/" element="首頁">首頁</Route>
+                <Route path="/" element={<Home />}></Route>
                 <Route path="/signin" element={<Signin />}>註冊登入</Route>
             </Routes>
         </BrowserRouter>  
