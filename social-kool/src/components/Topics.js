843ce6e47fdaabdad6481f7ad2e5becf6fd8ada5
Chi Chen <stephchen55@gmail.com>
2023-07-18 02:01:23 +0800

    讀取文章類別

--- social-kool/src/components/Topics.js
+++ social-kool/src/components/Topics.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import firebase from '../utils/firebase';
+import 'firebase/compat/firestore'
+import { List } from 'semantic-ui-react';
 
 export default function Topics(){
+    const [topics, setTopics] = React.useState([]);
+    React.useEffect(()=>{
+        firebase
+        .firestore()
+        .collection("topics")
+        .get()
+        .then((collectionSnapshot)=>{
+            const data = collectionSnapshot.docs.map(doc=>{
+                return doc.data();
+            })
+            setTopics(data);
+        })
+    })
     return (
-        "Topics"
+        <List>
+            {topics.map(topic=>{
+                return(
+                    <List.Item>{topic.name}</List.Item>
+                )
+            })}
+        </List>
     )
 }
