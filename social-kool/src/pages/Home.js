9f994aa166bad24e84ba6c6bd57f84ef843e5eff
Chi Chen <stephchen55@gmail.com>
2023-07-18 01:52:28 +0800

    add Topics.js

--- social-kool/src/pages/Home.js
+++ social-kool/src/pages/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import Topics from '../components/Topics';
 import { Grid } from 'semantic-ui-react';
 
 export default function Home(){
     return (
         <Grid>
             <Grid.Row>
-                <Grid.Column width={3}>主題</Grid.Column>
-                <Grid.Column width={10}>貼文</Grid.Column>
-                <Grid.Column width={3}>留白</Grid.Column>
+                <Grid.Column width={3}>
+                    <Topics />
+                </Grid.Column>
+                <Grid.Column width={10}>
+                    貼文
+                </Grid.Column>
+                <Grid.Column width={3}>
+                    留白
+                </Grid.Column>
             </Grid.Row>
 
         </Grid>
