8182588ac8353ce61504096b9e3b0e477d648942
Chi Chen <stephchen55@gmail.com>
2023-07-18 01:51:03 +0800

    add components

--- social-kool/src/pages/Home.js
+++ social-kool/src/pages/Home.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 
 export default function Home(){
-    return "Homeeeee"
+    return (
+        <Grid>
+            <Grid.Row>
+                <Grid.Column width={3}>主題</Grid.Column>
+                <Grid.Column width={10}>貼文</Grid.Column>
+                <Grid.Column width={3}>留白</Grid.Column>
+            </Grid.Row>
+
+        </Grid>
+    )
 }
