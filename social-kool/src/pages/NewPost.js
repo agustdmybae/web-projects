f7ca91b70ef76ed0a93b2457c7ff67742819f9f7
Chi Chen <stephchen55@gmail.com>
2023-07-19 16:42:52 +0800

    新增貼文發佈功能

--- social-kool/src/pages/NewPost.js
+++ social-kool/src/pages/NewPost.js
@@ -2,8 +2,10 @@ import React from "react";
 import firebase from "../utils/firebase";
 import 'firebase/compat/firestore';
 import { Container, Header, Form, Image, Button } from "semantic-ui-react";
+import {useNavigate} from 'react-router-dom';
 
 export default function NewPost(){
+    const navigate = useNavigate();
     const [title, setTitle] = React.useState("");
     const [content, setContent] = React.useState("");
     const [topics, setTopics] = React.useState([]);
@@ -31,11 +33,30 @@ export default function NewPost(){
     })
 
     const previewURL = image ? URL.createObjectURL(image) : "https://react.semantic-ui.com/images/wireframe/image.png"
+    //送出文章function
+    function onSubmit(){
+        const documentRef = firebase.firestore().collection("posts").doc();
+        documentRef.set({
+            title,
+            content,
+            topic: topicName,
+            createdAt: firebase.firestore.Timestamp.now(),
+            author:{
+                displayName: firebase.auth().currentUser.displayName || "",
+                photoURL: firebase.auth().currentUser.photoURL || "",
+                uid: firebase.auth().currentUser.uid,
+                email: firebase.auth().currentUser.email
+            },
+        }).then(()=>{
+            navigate('/');
+        })
+
+    }
 
     return (
         <Container>
             <Header>發表文章</Header>
-            <Form>
+            <Form onSubmit={onSubmit}>
                 <Image src={previewURL} size="small" floated="left"/>
                 <Button basic as="label" htmlFor="post-image">上傳文章圖片</Button>
                 <Form.Input 
