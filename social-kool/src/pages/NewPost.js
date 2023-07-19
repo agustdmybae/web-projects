6297e55073b50cc467da3e23e9632bcea3a42a21
Chi Chen <stephchen55@gmail.com>
2023-07-19 15:45:59 +0800

    add 文章主題選單

--- social-kool/src/pages/NewPost.js
+++ social-kool/src/pages/NewPost.js
@@ -1,5 +1,53 @@
+import React from "react";
+import firebase from "../utils/firebase";
+import 'firebase/compat/firestore';
+import { Container, Header, Form } from "semantic-ui-react";
+
 export default function NewPost(){
+    const [title, setTitle] = React.useState("");
+    const [content, setContent] = React.useState("");
+    const [topics, setTopics] = React.useState([]);
+
+    React.useEffect(()=>{
+        firebase
+        .firestore()
+        .collection("topics")
+        .get()
+        .then((collectionSnapshot)=>{
+            const data = collectionSnapshot.docs.map(doc=>{
+                return doc.data();
+            })
+            setTopics(data);
+        })
+    })
+
+    const options = topics.map(topic=>{
+        return({
+            text:topic.name,
+            value:topic.name,
+        }) 
+    })
+
     return (
-        "POST"
+        <Container>
+            <Header>發表文章</Header>
+            <Form>
+                <Form.Input 
+                    placeholder='輸入文章標題' 
+                    value={title} 
+                    onChange={(e)=>setTitle(e.target.value)}
+                />
+                <Form.TextArea
+                    placeholder='輸入文章內容' 
+                    value={content} 
+                    onChange={(e)=>setContent(e.target.value)}
+                />
+                <Form.Dropdown
+                    placeholder='選擇文章主題'
+                    options={options}
+                    selection
+                />
+            </Form>
+        </Container>
     )
 }
