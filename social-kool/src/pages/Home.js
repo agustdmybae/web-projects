d996423a16c278382720ad082545dc3b5ac7b4e9
Chi Chen <stephchen55@gmail.com>
2023-07-22 01:37:27 +0800

    modify Home.js

--- social-kool/src/pages/Home.js
+++ social-kool/src/pages/Home.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import Topics from '../components/Topics';
+import firebase from '../utils/firebase';
+import 'firebase/compat/firestore';
+import Topics from '../components/Topics'
 import { Grid } from 'semantic-ui-react';
 
-export default function Home(){
-    return (
+export default function Posts(){
+    const [posts, setPosts] = React.useState([]);
+    React.useEffect(()=>{
+        firebase.firestore().collection("posts").get().then((collectionSnapshot)=>{
+            const data = collectionSnapshot.docs.map(docSnapShot => {
+                return docSnapShot.data();
+            })
+            setPosts(data);
+        })
+    })
+    return(
         <Grid>
             <Grid.Row>
                 <Grid.Column width={3}>
-                    <Topics />
+                    <Topics/>
                 </Grid.Column>
-                <Grid.Column width={10}>
-                    貼文
-                </Grid.Column>
+                <Grid.Column width={10}>{
+                    posts.map(post=>{
+                        return <p>{post.title}</p>
+                    })
+                }</Grid.Column>
                 <Grid.Column width={3}>
                     留白
                 </Grid.Column>
-            </Grid.Row>
 
+            </Grid.Row>
         </Grid>
     )
+
 }
