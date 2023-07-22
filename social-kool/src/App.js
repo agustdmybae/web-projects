6fc084bb963ae73ff14857022f56589edba5cf37
Chi Chen <stephchen55@gmail.com>
2023-07-22 17:21:12 +0800

    add Post page

--- social-kool/src/App.js
+++ social-kool/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Signin from './pages/Signin'
 import Home from './pages/Home'
 import NewPost from './pages/NewPost';
+import Post from './pages/Post';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -13,7 +14,7 @@ export default function App() {
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/signin" element={<Signin />}>註冊登入</Route>
                 <Route path="/new-post" element={<NewPost />}>註冊登入</Route>
-                <Route path="/posts/:postId">Hello</Route>
+                <Route path="/posts/:postId" element={<Post />}></Route>
             </Routes>
         </BrowserRouter>  
     )
