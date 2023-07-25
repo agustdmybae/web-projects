04143128a18a995ed6d83ce5b1100166fb77167b
Chi Chen <stephchen55@gmail.com>
2023-07-25 13:00:28 +0800

    add Header.js

--- concert-view/src/App.js
+++ concert-view/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
     return (
-       "Heeee"
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </BrowserRouter> 
     )
 }
---
+++ concert-view/src/Header.js
@@ -0,0 +1,18 @@
+import { Menu, Search } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+export default function Header(){
+    return (
+        <Menu>
+            <Menu.Item as={Link} to="/">
+                CONCERT VIEW
+            </Menu.Item>
+            <Menu.Menu position="right">
+                <Menu.Item>
+                    註冊/登入
+                </Menu.Item>
+            </Menu.Menu>
+        </Menu>
+    );
+}
